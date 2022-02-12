cdea9e63c2ab92149751a2abd7872bcf8de13b7f
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2022-02-12 21:38:55 +0300

    app window root viewcontroller set

--- GetirTodo/Application/ToDoItemList/ToDoItemListVC.swift
+++ GetirTodo/Application/ToDoItemList/ToDoItemListVC.swift
@@ -11,7 +11,7 @@ class ToDoItemListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .cyan
         // Do any additional setup after loading the view.
     }
 
