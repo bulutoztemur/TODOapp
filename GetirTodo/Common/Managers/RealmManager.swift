b06724883eef556e9bbc1cbcae578b42343ced27
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2022-02-12 22:47:49 +0300

    Realm sdk installed

---
+++ GetirTodo/Common/Managers/RealmManager.swift
@@ -0,0 +1,8 @@
+//
+//  RealmManager.swift
+//  GetirTodo
+//
+//  Created by alaattinbulut on 12.02.2022.
+//
+
+import Foundation
---
+++ GetirTodo/Common/Models/ToDoListItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoListItem.swift
+//  GetirTodo
+//
+//  Created by alaattinbulut on 12.02.2022.
+//
+
+import Foundation
